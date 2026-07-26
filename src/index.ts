8e34404579afff7878a9eb8ae3465f263216fd4f
Kyaw Paing Oo <82670447+Kyawpaingoo@users.noreply.github.com>
2026-07-26 12:38:13 +0700

    added/better-auth

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import { Hono } from 'hono'
 import postRoutes from './router/post.route'
 import { logger } from 'hono/logger'
+import { auth } from './lib/auth'
 
 const app = new Hono()
 
 app.use('*', logger())
 
 app.get('/', (c) => c.json('API is running'))
+
+app.on(['POST', 'GET'], '/api/auth/*', (c) => auth.handler(c.req.raw))
 
 app.route('/posts', postRoutes)
 
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,15 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { dash } from "@better-auth/infra";
 import { db } from "../db";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg"
   }),
+  plugins: [
+    dash({
+      apiKey: process.env.BETTER_AUTH_API_KEY,
+    }),
+  ],
 });
